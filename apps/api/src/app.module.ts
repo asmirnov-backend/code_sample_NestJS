02172de5962bc7fcc186636ef8c1a50eaa290175
Andrew Smirnov <asmirnov@upteam.com>
2022-05-27 19:42:39 +0300

    Add nest config

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { BroadstreetModule } from './broadstreet/broadstreet.module';
 
 @Module({
-  imports: [BroadstreetModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), BroadstreetModule],
   controllers: [AppController],
   providers: [],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { LoggingRequestInterceptor } from './logging-request.interceptor';
 
 async function bootstrap() {
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const config = appContext.get(ConfigService);
+  const httpPort = Number(config.get<string>('APP_PORT')) || 3000;
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new LoggingRequestInterceptor());
-  await app.listen(3000);
+  await app.listen(httpPort);
 }
 bootstrap();
