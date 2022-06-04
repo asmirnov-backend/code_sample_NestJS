dd6cec254d732b0726be48cb9a6d6dbacc3f2d8b
Andrew Smirnov <asmirnov@upteam.com>
2022-06-04 22:38:18 +0300

    Add graphql, use it to metadata module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,27 @@
+import { join } from 'path';
+
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 
 import { AppController } from './app.controller';
 import { BroadstreetModule } from './broadstreet/broadstreet.module';
+import { MetadataModule } from './metadata/metadata.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), BroadstreetModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./apps/api/**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), '/apps/api/src/graphql.schema.ts'),
+      },
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    BroadstreetModule,
+    MetadataModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
