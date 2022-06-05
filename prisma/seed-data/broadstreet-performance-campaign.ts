8f732994ddbe5788fd6576bf51fbcdf1bf860918
Andrew Smirnov <asmirnov@upteam.com>
2022-06-05 21:33:21 +0300

    Fix seed

--- prisma/seed-data/broadstreet-performance-campaign.ts
+++ prisma/seed-data/broadstreet-performance-campaign.ts
@@ -2,17 +2,17 @@ import { broadstreet_performance_campaign } from '@prisma/client';
 
 export const performanceCampaignsSeed: broadstreet_performance_campaign[] = [
   {
-    campaign_id: BigInt(1),
+    campaign_id: 1,
     date: new Date('2022-01-01'),
-    views: BigInt(10),
-    hovers: BigInt(1),
-    clicks: BigInt(1),
+    views: 10,
+    hovers: 1,
+    clicks: 1,
   },
   {
-    campaign_id: BigInt(1),
+    campaign_id: 1,
     date: new Date('2022-01-02'),
-    views: BigInt(10),
-    hovers: BigInt(1),
-    clicks: BigInt(1),
+    views: 10,
+    hovers: 1,
+    clicks: 1,
   },
 ];
--- prisma/seed-data/campaigns.ts
+++ prisma/seed-data/campaigns.ts
@@ -1,17 +1,16 @@
 import { AnalyticsType, Campaign } from '@prisma/client';
-import { Decimal } from '@prisma/client/runtime';
 
 export const campaignsSeed: Campaign[] = [
   {
-    id: BigInt(1),
+    id: 1,
     originId: 1,
     name: 'name 1',
     friendlyName: 'friendlyName',
-    spendRate: new Decimal(1.1),
+    spendRate: 1.1,
     spendType: 'CPM',
     contractStart: '2022-01-01',
     contractEnd: '2022-02-01',
     type: AnalyticsType.BROADSTREET,
-    advertiserId: BigInt(1),
+    advertiserId: 1,
   },
 ];
